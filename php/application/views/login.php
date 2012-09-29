0ced62532a22aa3aa60367fc8c05e0a94cd8e6d6
Dominic Deflize <deflized@arcor.de>
2012-09-29 14:08:30 +0800

    get it running

--- php/application/views/login.php
+++ php/application/views/login.php
@@ -6,8 +6,8 @@
 	<title>Appoint.ee</title>
 	
 	<link rel="stylesheet" href="http://jquerymobile.com/demos/1.1.1/css/themes/default/jquery.mobile-1.1.1.css" />
-  <?php $this->load->helper('url') ?>
-	<link rel="stylesheet" href="<?=base_url() ?>asset/style.css" />
+  
+	<link rel="stylesheet" href="</asset/style.css" />
 	
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 	<script src="http://jquerymobile.com/demos/1.1.1/js/jquery.mobile-1.1.1.js"></script>
