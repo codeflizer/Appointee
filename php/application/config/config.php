a5eb92fe092ff0afc84ab5a98737255100fd5d91
Dominic Deflize <deflized@arcor.de>
2012-09-29 13:47:40 +0800

    get it running

--- php/application/config/config.php
+++ php/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'appointeedev-team42.rhcloud.com/';
+$config['base_url']	= 'appointeedev-team42.rhcloud.com';
 
 /*
 |--------------------------------------------------------------------------
