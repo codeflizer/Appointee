b4bde754e556e509750ea3abcc3a915b08b08c43
Dominic Deflize <deflized@arcor.de>
2012-10-01 04:58:11 +0800

    gogo

--- php/application/controllers/home.php
+++ php/application/controllers/home.php
@@ -23,7 +23,7 @@ class Home extends CI_Controller {
 	
 	public function logout(){
 	 $this->session->sess_destroy();
-     redirect(login);
+     redirect('login');
     }
 	
      public function history(){
