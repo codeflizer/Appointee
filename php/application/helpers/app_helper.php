c952eb6c7689488f48a386f0c2f15876a1bbd840
Dominic Deflize <deflized@arcor.de>
2012-10-02 23:10:59 +0800

    blubb

--- php/application/helpers/app_helper.php
+++ php/application/helpers/app_helper.php
@@ -8,10 +8,10 @@ if ( ! function_exists('includeCss'))
 	    $ci =& get_instance();
         $ci->load->helper('url');
 		
-	echo '<link rel="stylesheet" href="'.base_url().'asset/jquery.mobile-1.1.1.css" />
+	echo '<link rel="stylesheet"  type="text/css"  href="'.base_url().'asset/jquery.mobile-1.1.1.css" />
 	<script src="'.base_url().'asset/jquery-1.7.1.min.js"></script>
 	<script src="'.base_url().'asset/jquery.mobile-1.1.1.js"></script>
-	<link rel="stylesheet" href="'.base_url().'asset/style.css" />';
+	<link rel="stylesheet"  type="text/css" href="'.base_url().'asset/style.css" />';
 	
 	
 	}
