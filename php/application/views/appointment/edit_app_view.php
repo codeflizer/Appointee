5cee15583ac2489dfb60bc6585f6e791b399fdaa
Max <mjwich.2012@exchange.smu.edu.sg>
2012-10-26 16:05:14 +0800

    Change of participant selection

--- php/application/views/appointment/edit_app_view.php
+++ php/application/views/appointment/edit_app_view.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Appoint.ee</title>
 	
 	<?php includeCss() ?>
@@ -12,7 +12,9 @@
 	<script type="text/javascript" src="<?=base_url()?>asset/datebox/jqm-datebox.mode.datebox.min.js"></script>
 	<script type="text/javascript" src="<?=base_url()?>asset/datebox/jquery.mobile.datebox.i18n.en_US.utf8.js"></script>
 	<script type="text/javascript" src="<?=base_url()?>asset/datebox/jqm-datebox-1.1.0.mode.durationbox.js"></script>
-
+    <script type="text/javascript" src="https://appointeedev-team42.rhcloud.com/asset/autosuggest/jquery.tokeninput.js"></script>
+   
+    <link rel="stylesheet" href="https://appointeedev-team42.rhcloud.com/asset/autosuggest/token-input.css" type="text/css" />
 
 
 </head>
@@ -35,6 +37,43 @@
     </div>
 	<div data-role="content" style="padding: 15px">
 		<ul data-role="listview" data-divider-theme="d" data-inset="false">
+			<li data-role="list-divider" role="heading">
+			Participants
+			</li>
+			<li data-theme="c">
+				<div class="as_participants">
+					<input type="text" id="demo-input-local-custom-formatters" name="blah" data-role="none" class="asg"/>
+						<script type="text/javascript">
+						$(document).ready(function() {
+							// entries
+							$("#demo-input-local-custom-formatters").tokenInput([{
+								"first_name": "Arthur",
+								"last_name": "Godfrey",
+								"email": "arthur_godfrey@nccu.edu",
+								"url": "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png"
+							},
+							{
+								"first_name": "Adam",
+								"last_name": "Johnson",
+								"email": "wravo@yahoo.com",
+								"url": "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png"
+							}
+						  ], {
+							  propertyToSearch: "first_name",
+							  resultsFormatter: function(item){ return "<li>" + "<img src='" + item.url + "' title='" + item.first_name + " " + item.last_name + "' height='25px' width='25px' />" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.first_name + " " + item.last_name + "</div><div class='email'>" + item.email + "</div></div></li>" },
+							  tokenFormatter: function(item) { return "<li><p>" + item.first_name + " <b>" + item.last_name + "</b></p></li>" },
+							  // pre-populated entries
+							  prePopulate: [
+								{"first_name": "Andre",
+									"last_name": "Henderson",
+									"email": "andre.henderson@globo.com",
+									"url": "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png"}
+								]
+						  } );
+						});
+						</script>
+				</div>
+			</li>	
 			<li data-role="list-divider" role="heading">
 				General Information
 			</li>
