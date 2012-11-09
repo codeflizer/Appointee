2c69643b72932e44a549860f42045d9ceec995d8
Dominic Deflize <deflized@arcor.de>
2012-11-09 17:36:18 +0800

    change time format

--- php/application/views/history/app_view.php
+++ php/application/views/history/app_view.php
@@ -56,7 +56,7 @@
 			<li data-theme="c" data-icon="gear">	
      <h3><small> <?php echo date_format($starttime, 'D dS \o\f F \, Y'); ?></small></h3>, <br />  <p><strong><?php echo date_format($starttime, 'g:i a'); ?> - <?php echo date_format($endtime, 'g:i a');  ?> </strong></p>
 			<p class="ui-li-aside">in <strong>2</strong> days</p>
-			<p align=right> <?php echo $timeslot['ack'] ?> Acknowledgements </p>
+			<p align=right> <?php echo $timeslot['number_of_ack'] ?> Acknowledgements </p>
       </li>
       
 				
